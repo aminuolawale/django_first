86858a6010c67eea263ad7996f2131fc2f9d2ef4
Mohammed Aminu <aminuolawale@bitbucket.org>
2020-06-06 00:50:32 +0100

    updated settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, "staticfiles")
-STATIC_ROOT = '/static_files'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL =  "login"
